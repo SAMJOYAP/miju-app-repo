dcf9d144c342540b0bef80a11c9c6d2f8ac2a4ae
미주 <miju@MacBook-Pro-2.local>
2026-02-09 11:58:56 +0900

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ def hello():
             <h1>practice app</h1>
             <p>Built by Github Actions, deployed by Argo CD.</p>
             <p>Secured by DevSecOps Pipeline!</p>
-        <body>
+        </body>
     </html>
     '''
+
 @app.route('/healthz')
 def health():
     return 'ok', 200
 
-if __name == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
